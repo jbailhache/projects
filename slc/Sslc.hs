47035aed21b12602848af3b7be7d3aea0bdbfe57
Jacques Bailhache <jacques.bailhache@gmail.com>
2016-05-20 22:49:13 +0200

    From CL to SLC + Haskell code

---
+++ slc/Sslc.hs
@@ -0,0 +1,85 @@
+module Sslc where
+
+ data Proof = AXM
+			| SMB 
+  			| DB0 
+  			| DBS Proof
+  			| DBL Proof
+  			| APL Proof Proof
+  			| LTR Proof Proof
+	deriving (Eq, Show)
+
+ data ProofWithEq = ProofWithEq Proof [Proof]
+	deriving (Eq, Show)
+
+ proofWithoutEq :: ProofWithEq -> Proof
+ proofWithoutEq (ProofWithEq x l) = x
+
+ data Side = LeftSide | RightSide
+	deriving (Eq, Show)
+
+ shift :: Proof -> Proof -> Proof
+ shift _ AXM = AXM
+ shift _ SMB = SMB
+ shift _ DB0 = DB0
+ shift u (DBS x) = if u == DBS x then DBS u else DBS (shift u x)
+ shift u (DBL x) = DBL (shift (DBS u) x)
+ shift u (APL x y) = APL (shift u x) (shift u y)
+ shift u (LTR x y) = LTR (shift u x) (shift u y)
+
+ subst :: Proof -> Proof -> Proof -> Proof
+ subst1 :: Proof -> Proof -> Proof -> Proof
+ subst u a b = if u == a then b else (if DBS u == a then u else subst1 u a b)
+ subst1 _ AXM b = AXM
+ subst1 _ SMB _ = SMB
+ subst1 _ DB0 _ = DB0
+ subst1 u (DBS x) b = DBS (subst u x b)
+ subst1 u (DBL x) b = DBL (subst (DBS u) x (shift DB0 b))
+ subst1 u (APL x y) b = APL (subst u x b) (subst u y b)
+ subst1 u (LTR x y) b = LTR (subst u x b) (subst u y b)
+
+ red1 :: Proof -> Proof
+ red1 AXM = AXM
+ red1 SMB = SMB
+ red1 DB0 = DB0
+ red1 (DBS x) = DBS (red1 x)
+ red1 (DBL x) = DBL (red1 x)
+ red1 (APL (DBL x) y) = subst DB0 x y
+ red1 (APL x y) = APL (red1 x) (red1 y)
+ red1 (LTR x y) = LTR (red1 x) (red1 y)
+
+ red2 :: ProofWithEq -> ProofWithEq
+ red2 (ProofWithEq x l) = ProofWithEq (red1 x) (x : l)
+
+ red3 :: ProofWithEq -> ProofWithEq
+ red3 (ProofWithEq x l) = if elem x l then ProofWithEq x l else red3 (red2 (ProofWithEq x l))
+
+ red :: Proof -> Proof
+ red x = proofWithoutEq (red3 (ProofWithEq x []))
+ -- red x = let (ProofWithEq x l) = red3 (ProofWithEq x []) in x
+ -- red x = if red1 x == x then x else red (red1 x)
+
+ side :: Side -> Proof -> Proof -> Proof -> Proof
+ side LeftSide a b AXM = a
+ side RightSide a b AXM = b
+ side _ _ _ SMB = SMB
+ side _ _ _ DB0 = DB0
+ side s a b (DBS x) = DBS (side s a b x)
+ side s a b (DBL x) = DBL (side s a b x)
+ side s a b (APL x y) = APL (side s a b x) (side s a b y)
+ side s a b (LTR x y) = if red (side LeftSide a b x) == red (side LeftSide a b y) then (side RightSide a b (if s == LeftSide then x else y)) else LTR x y
+
+ axl = SMB
+ axr = APL SMB SMB
+
+ left = side LeftSide axl axr
+ right = side RightSide axl axr
+
+ proves x = do
+  putStrLn ("The proof " ++ show x ++ " proves " ++ show (left x) ++ " = " ++ show (right x))
+
+ test = do
+  proves (LTR (LTR AXM SMB) (APL SMB AXM))
+  proves (LTR (LTR AXM SMB) (APL AXM SMB))
+  proves (LTR (LTR AXM SMB) (APL AXM AXM))
+
