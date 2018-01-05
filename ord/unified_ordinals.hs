42c87d7138c66fb3bd61db9eebfa0ebe3dc4d197
Jacques Bailhache <jacques.bailhache@gmail.com>
2018-01-05 17:37:32 +0100

    Unified definition of ordinals as least ordinal strictly greater than all ordinals in a given set

---
+++ ord/unified_ordinals.hs
@@ -0,0 +1,25 @@
+module Ords where
+
+ -- Ordinals
+ data Ord 
+  = Zero
+  | One
+  | W0
+  | W1
+  | W2
+  | Sup Ord (Ord -> Ord)
+
+ two = Sup One (\x -> One)
+ three = Sup One (\x -> two)
+
+ suc a = Sup One (\x -> a)
+
+ -- f^a(x)
+ fpower0 f Zero x = x
+ -- fpower0 f (Suc a) x = f (fpower0 f a x)
+ fpower0 f (Sup One s) x = f (fpower0 f (s Zero) x)
+ -- fpower0 f (Lim s) x = Lim (\n -> fpower0 f (s n) x)
+ fpower0 f (Sup W0 s) x = Sup W0 (\n -> fpower0 f (s n) x) 
+
+ w_times_2 = Sup W0 (\n -> fpower0 suc n W0)
+
