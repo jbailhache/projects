984e2af6e6a2fcf897d96c58b7d90c22ee87ee06
Jacques Bailhache <jacques.bailhache@gmail.com>
2018-01-20 04:28:05 +0100

    RHS0 rewriting in Scheme - corrections

--- ord/rhsz.scm
+++ ord/rhsz.scm
@@ -1,6 +1,6 @@
 
 (define mp (lambda (x)
-	(if (cons? x)
+	(if (pair? x)
 		(if (eq? (car x) ':) (list (mp (cdr x)))
 			 (cons (mp (car x)) (mp (cdr x))) )
 		x ) ))
@@ -8,24 +8,24 @@
 (eval (mp '(begin
 
 (define r2a : lambda (l) : 
- if (not : cons? l) l :
- if (cons? : car l) (r2 : append (car l) : cdr l) :
+ if (not : pair? l) l :
+ if (pair? : car l) (r2 : append (car l) : cdr l) :
  if (eq? (car l) 'H) (cons (cadr l) : cons (list (cadr l) (caddr l)) : cdddr l) :
  if (eq? (car l) 'R1) (cons (cadr l) : cons (cadr l) : cddr l) :
  if (eq? (car l) 'R2) (cons (cadr l) : cons (caddr l) : cons (cadr l) : cons (caddr l) : cdddr l) 
  l)
 
 (define r : lambda (l) :
- if (not : cons? l) l :
+ if (not : pair? l) l :
  let ((l1 (map r l))) : 
- if (cons? : car l1) (append (car l1) (cdr l1)) 
+ if (pair? : car l1) (append (car l1) (cdr l1)) 
  l1)
 
 (define r2 : lambda (l) : r : r2a l)
 
 (define loopr2 : lambda (n l) :
  begin (display l) (newline) :
- if (eq? n 0) '() :
+ if (equal? n 0) '() :
  loopr2 (- n 1) (r2 l))
 
 )))
